5ac4ae35863025cecf9004d260f6392c4ef8ace7
Jagannath Hari <jagannathhari17@gmail.com>
2024-05-24 14:57:45 +0530

    Refactored the code

--- src/main.c
+++ src/main.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "parser.h"
+#include "support.h"
 #include <raylib.h>
 #include <raymath.h>
-#include "parser.h"
-#include "utils.h"
+
 #define W 1970
 #define H 1000
 
 #define TITLE "Conway Game Of Life"
 
 #define SIZE 20
-#define FPS  10
+#define FPS 10
 
 #define MAX_FILEPATH_SIZE 2048
 
@@ -40,11 +41,10 @@ bool should_die(int total_neougher) {
 
 bool can_become_alive(int total_neougher) { return total_neougher == 3; }
 
-
-void fill_random(char **grid , int rows , int cols){
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<cols;j++){
-            grid[i][j] = GetRandomValue('0','1');
+void fill_random(char **grid, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            grid[i][j] = GetRandomValue('0', '1');
         }
     }
 }
@@ -52,25 +52,20 @@ void fill_random(char **grid , int rows , int cols){
 void update_state(char **state, char cell_val, int row, int col,
                   int total_neighbour) {
     if (cell_val == '1') {
-        if (should_die(total_neighbour)) {
+        if (should_die(total_neighbour))
             state[row][col] = '0';
-        } else {
+        else
             state[row][col] = '1';
-        }
-    } else {
-        if (can_become_alive(total_neighbour)) {
-            state[row][col] = '1';
-        }
+        return;
     }
+    if (can_become_alive(total_neighbour))
+        state[row][col] = '1';
 }
 
-void draw_cell(char cell_val, int x, int y,int size) {
-
-    if (cell_val == '1') {
-        DrawRectangle((x - 1) * size, (y - 1) * size, size, size, RED);
-    } else {
-       DrawRectangleLines((x - 1) * size, (y - 1) * size, size, size, BLACK);
-    }
+void draw_cell(char cell_val, int x, int y, int size) {
+    (cell_val == '1')
+        ? DrawRectangle((x - 1) * size, (y - 1) * size, size, size, RED)
+        : DrawRectangleLines((x - 1) * size, (y - 1) * size, size, size, BLACK);
 }
 
 void update_grid(char **grid, char **state, int rows, int cols) {
@@ -79,7 +74,7 @@ void update_grid(char **grid, char **state, int rows, int cols) {
     }
 }
 
-void fill_cell_on_click(char **grid, int button,int size) {
+void fill_cell_on_click(char **grid, int button, int size) {
     if (IsMouseButtonDown(button)) {
         Vector2 pos = GetMousePosition();
         int x = pos.x / size + 1;
@@ -88,23 +83,21 @@ void fill_cell_on_click(char **grid, int button,int size) {
     }
 }
 
-int main(int argc , char **argv) {
-    World world; 
+int main(int argc, char **argv) {
+    World world;
     int rows = H / SIZE;
     int cols = W / SIZE;
     int size = SIZE;
-    if(argc==2){
-        create_world(argv[1],&world);
+    if (argc == 2) {
+        create_world(argv[1], &world);
         rows = world.height;
         cols = world.width;
-        size = max(1,min(H/rows,W/cols));
-    }else{
+        size = max(1, min(H / rows, W / cols));
+    } else {
         world.height = rows;
         world.width = cols;
         world.grid = allocate_grid(rows + 2, cols + 2, '0');
     }
-
-    
 
     bool start = false;
 
@@ -116,8 +109,8 @@ int main(int argc , char **argv) {
 
     while (!WindowShouldClose()) {
 
-        fill_cell_on_click(world.grid, MOUSE_BUTTON_LEFT,size);
-        fill_cell_on_click(world.grid, MOUSE_BUTTON_RIGHT,size);
+        fill_cell_on_click(world.grid, MOUSE_BUTTON_LEFT, size);
+        fill_cell_on_click(world.grid, MOUSE_BUTTON_RIGHT, size);
 
         if (IsKeyReleased(KEY_SPACE))
             start = !start;
@@ -130,7 +123,7 @@ int main(int argc , char **argv) {
                     int total_neh = get_total_neighour(world.grid, i, j);
                     update_state(state, world.grid[i][j], i, j, total_neh);
                 }
-                draw_cell(world.grid[i][j], j, i,size);
+                draw_cell(world.grid[i][j], j, i, size);
             }
         }
         EndDrawing();
@@ -140,11 +133,16 @@ int main(int argc , char **argv) {
         if (IsKeyReleased(KEY_C)) {
             clear_grid(world.grid, rows, cols, '0');
             clear_grid(state, rows, cols, '0');
-        }else if(IsKeyDown(KEY_R)){
-            fill_random(world.grid,rows,cols);
+        } else if (IsKeyDown(KEY_R)) {
+            fill_random(world.grid, rows, cols);
         }
     }
     CloseWindow();
+    for (int i = 0; i < rows + 2; i++) {
+        free(world.grid[i]);
+        free(state[i]);
+    }
+    free(world.grid);
+    free(state);
     return 0;
 }
-
